4731762d88e6472b80a39d659f74b9bf6131a8dc
Ansh Singh <anshsingh.14.lko@gmail.com>
2018-08-14 18:36:13 +0530

    Add files via upload

---
+++ m5note-vendor-blobs.mk
@@ -0,0 +1,14 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5note/vendor,system/vendor)
+
